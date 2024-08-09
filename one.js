83bcd9ef4031313292ba455f9b0ae9d41da2da1d
rvkvivekr <85954214+rvkvivekr@users.noreply.github.com>
2024-08-09 18:25:44 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.json(data);   
+});
+
+app.post('/Plans/estimate', function(req, res) {
+    data.push(req.body);
+    res.end(JSON.stringify(data));
+});
+
+var data = [{
+  name: 'Mobile update8',
+  id: '8',
+  amount: '80000'
+}];
+
+app.listen(8080, function() {
+    console.log('Service is running');
+});
